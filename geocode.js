f66e1adf6666bbe573383d2c6ea6b1755a4b0d14
Bert Spaan <bert.spaan@gmail.com>
2020-07-27 14:22:28 +0200

    added cache

--- geocode.js
+++ geocode.js
@@ -1,25 +1,61 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
 const H = require('highland')
 const axios = require('axios')
 
+const CACHE_FILENAME = './geocoding-cache.json'
+let geocodingCache = {}
+try {
+  geocodingCache = require(CACHE_FILENAME)
+} catch (err) {
+  console.error('Cache not found, creating after first run')
+}
+
+function writeCacheToFile () {
+  console.error('Writing cache to file')
+  fs.writeFileSync(CACHE_FILENAME, JSON.stringify(geocodingCache, null, 2))
+}
+
+let i = 0
 async function geocode (servicePoint) {
-  const address = servicePoint.address
-  const url = `https://geodata.nationaalgeoregister.nl/locatieserver/v3/free?fq=type:adres%20AND%20bron:BAG&q=${address}`
-
-  const response = await axios.get(url)
-
   let geometry
-  if (response.data && response.data.response && response.data.response.docs) {
-    const doc = response.data.response.docs[0]
-    const wktPoint = doc.centroide_ll
-    const match = wktPoint.match(/\d+\.\d+/g)
+  const address = servicePoint.address
 
-    geometry = {
-      type: 'Point',
-      coordinates: [parseFloat(match[0]), parseFloat(match[1])]
+  if (geocodingCache[address]) {
+    geometry = geocodingCache[address]
+  } else {
+    const url = `https://geodata.nationaalgeoregister.nl/locatieserver/v3/free?fq=type:adres%20AND%20bron:BAG&q=${encodeURIComponent(address)}`
+
+    let data
+    try {
+      const response = await axios.get(url)
+      data = response.data
+    } catch (err) {
+      console.error(url)
+      console.error(err.message)
+    }
+
+    if (data && data.response && data.response.docs) {
+      const doc = data.response.docs[0]
+      const wktPoint = doc.centroide_ll
+      const match = wktPoint.match(/\d+\.\d+/g)
+
+      geometry = {
+        type: 'Point',
+        coordinates: [parseFloat(match[0]), parseFloat(match[1])]
+      }
+
+      geocodingCache[address] = geometry
     }
   }
+
+  if (i && i % 50 === 0) {
+    writeCacheToFile()
+  }
+
+  i++
+  console.error(`Geocoded address #${i}`)
 
   return {
     ...servicePoint,
@@ -27,11 +63,17 @@ async function geocode (servicePoint) {
   }
 }
 
-H(process.stdin)
+const ndjson = H(process.stdin)
   .split()
   .compact()
   .map(JSON.parse)
   .flatMap((servicePoint) => H(geocode(servicePoint)))
   .map(JSON.stringify)
   .intersperse('\n')
+
+ndjson
+  .observe()
+  .done(writeCacheToFile)
+
+ndjson
   .pipe(process.stdout)
--- geocode.sh
+++ geocode.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# ./geocode.js < service-points.ndjson > service-points-geocoded.ndjson
+cat service-points.ndjson | sort | uniq | ./geocode.js > service-points-geocoded.ndjson
 ../ndjson-to-geojson/ndjson-to-geojson.js < service-points-geocoded.ndjson > service-points.geojson
